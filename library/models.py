c33a9c9d0ac7d24ac38afb4c01ab7fb4d912989c
zanriks <arseniysharnin@gmail.com>
2025-12-21 23:20:11 +0700

    models.p2

--- library/models.py
+++ library/models.py
@@ -2,6 +2,36 @@ from django.db import models
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.TextField()
+    date_of_birth = models.DateField()
+    date_of_death = models.DateField()
+
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+        constraints = [
+            models.UniqueConstraint(
+                fields=['first_name', 'last_name'],
+                name='unique_author_name'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.first_name} ({self.last_name})"
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название издательства")
+
+    class Meta:
+        verbose_name = "Издательство"
+        verbose_name_plural = "Издательства"
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     BOOK_TYPE_CHOICES = [
         ('textbook', 'Учебник'),
@@ -9,11 +39,11 @@ class Book(models.Model):
     ]
 
     title = models.CharField(max_length=100, verbose_name = "Название")
-    author = models.CharField(max_length=100, verbose_name = "Автор книги")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name = 'books', verbose_name = "Автор книги")
     year = models.IntegerField(verbose_name = "Год издания")
     genre = models.CharField(max_length=100, verbose_name = "Жанр")
     category = models.CharField(max_length=100, verbose_name = "Категория")
-    publisher = models.CharField(max_length=100, verbose_name = "Издательство")
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, verbose_name = "Издательство")
     cover = models.ImageField(upload_to='covers/', null=True, blank=True, verbose_name = "Обложка")
     file = models.FileField(upload_to='books/', null=True, blank=True, verbose_name = "Фай книги")
     book_type = models.CharField(
@@ -35,24 +65,3 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.author}, {self.year})"
-
-
-class Author(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    bio = models.TextField()
-    date_of_birth = models.DateField()
-    date_of_death = models.DateField()
-
-    class Meta:
-        verbose_name = "<UNK>"
-        verbose_name_plural = "<UNK>"
-        constraints = [
-            models.UniqueConstraint(
-                fields=['first_name', 'last_name'],
-                name='unique_author_name'
-            )
-        ]
-
-    def __str__(self):
-        return f"{self.first_name} ({self.last_name})"
